798d211418ef7ffc3cbb52cd86dd04ce0d9ecf3e
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2022-05-30 12:44:07 -0300

    Create peoplecontroller on consumer

---
+++ Consumer/Consumer/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get() => await Task.Run(() => Ok("Controller Working!"));
+    }
+}
